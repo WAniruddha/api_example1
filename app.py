2a59a371e1858ff71af627c20d0cf8526422b2c4
Aniruddha Wadekar <114826006+WAniruddha@users.noreply.github.com>
2026-06-13 17:59:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import sys
 from fastapi import FastAPI
 import uvicorn
+import os
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"message": "API is running"}
 
 @app.get("/add")
-
 def add(x: float, y: float):
     return {"result": x + y}
 
@@ -15,13 +17,5 @@ def subtract(x: float, y: float):
     return {"result": x - y}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=9321)
-    print ("Server is running on http://localhost:9321")
-    
-    
-    """operation = sys.argv[1]
-    x = int(sys.argv[2])
-    y = int(sys.argv[3])
-    print(f"x:{x}, y:{y}, operation:{operation}")
-    print(f"value={add(x, y)}")"""
-
+    port = int(os.environ.get("PORT", 9321))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
